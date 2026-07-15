38f1bb85a449cb08813b95c2a0437ca51c8502c8
Irina Rogova <irinfoto@yandex.ru>
2026-07-15 12:16:35 +0300

    fix: исправлены импорты в helpers.py, используется Urls и Endpoints

--- helpers.py
+++ helpers.py
@@ -3,6 +3,7 @@ import random
 import string
 from data import Urls, Endpoints
 
+
 def generate_random_string(length):
     letters = string.ascii_lowercase
     random_string = ''.join(random.choice(letters) for i in range(length))
