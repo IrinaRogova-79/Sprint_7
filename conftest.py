cf39362b8de282d81c1b6a71d39fbba76c906cd6
Irina Rogova <irinfoto@yandex.ru>
2026-07-15 12:16:12 +0300

    fix: исправлены импорты в conftest.py, используется Urls, Endpoints, OrderData

--- conftest.py
+++ conftest.py
@@ -3,13 +3,15 @@ import requests
 from data import Urls, Endpoints, OrderData
 from helpers import register_new_courier_and_return_login_password
 
+
 @pytest.fixture
 def create_courier():
     login_pass = register_new_courier_and_return_login_password()
     yield login_pass
     if login_pass:
         login, password = login_pass[0], login_pass[1]
-        response = requests.post(f'{Urls.BASE_URL}{Endpoints.LOGIN_COURIER}', data={'login': login, 'password': password})
+        response = requests.post(f'{Urls.BASE_URL}{Endpoints.LOGIN_COURIER}', 
+                                 data={'login': login, 'password': password})
         if response.status_code == 200:
             courier_id = response.json().get('id')
             if courier_id:
@@ -20,6 +22,6 @@ def create_courier():
 def create_order():
     response = requests.post(f'{Urls.BASE_URL}{Endpoints.CREATE_ORDER}', json=OrderData.DEFAULT)
     track = response.json().get('track')
-    yield ORDER_DATA, track
+    yield OrderData.DEFAULT, track
     if track:
         requests.put(f'{Urls.BASE_URL}{Endpoints.CANCEL_ORDER}', json={'track': track})
